56a34a1acb6c6a29d450435f3291a66595f1b95f
Gabrielius <gabrielius.samsonas@gmail.com>
2025-09-10 00:00:27 +0300

    Simplify `DiscordTokenProvider` by removing redundant token caching logic.

--- FoodBot.Infrastructure/DiscordTokenProvider.cs
+++ FoodBot.Infrastructure/DiscordTokenProvider.cs
@@ -5,14 +5,5 @@ namespace FoodBot.Infrastructure;
 
 public sealed class DiscordTokenProvider(IOptions<AppOptions> dataOptions) : IDiscordToken
 {
-    private string? _token;
-
-    public string Token => GetToken();
-
-    private string GetToken()
-    {
-        if (_token is not null) return _token!;
-        _token = dataOptions.Value.DiscordToken;
-        return dataOptions.Value.DiscordToken;
-    }
+    public string Token => dataOptions.Value.DiscordToken;
 }
