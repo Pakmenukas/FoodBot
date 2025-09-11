606e9c4839d2b7a358d1d1351a6d9ab4057fac72
Gabrielius <gabrielius.samsonas@gmail.com>
2025-09-11 23:47:50 +0300

    Borders for add money form

--- foodbot-web/src/components/forms/AddMoneyForm.tsx
+++ foodbot-web/src/components/forms/AddMoneyForm.tsx
@@ -8,7 +8,7 @@ export default function AddMoneyForm({userId}: AddMoneyFormProps) {
     return (
         <form action={addMoney} className="flex gap-1">
             <input type="hidden" name="userId" value={userId ?? ""} />
-            <input type="text" className="input-sm w-16 p-1" name="amount" placeholder="Amount" step="0.01" required/>
+            <input type="text" className="input-sm border-primary border-1 rounded-field w-16 p-1" name="amount" placeholder="Amount" step="0.01" required/>
             <button type="submit" className="btn btn-primary rounded-field">Add</button>
         </form>
     )
